79a4af9d628209304a3dd32bc1c8023ccd1fe25d
Swann <swann.rouanet@gmail.com>
2025-05-23 06:22:00 +0200

    chore: rename AxolotlPetAccessor to AxolotlEntityAccessor

--- src/main/java/com/hezaerd/accessor/AxolotlEntityAccessors.java
+++ src/main/java/com/hezaerd/accessor/AxolotlEntityAccessors.java
@@ -1,6 +1,6 @@
 package com.hezaerd.accessor;
 
-public interface AxolotlPetAccessor extends AxolotlTameableAccessor, AxolotlShoulderAccessor {
+public interface AxolotlEntityAccessors extends AxolotlTameableAccessor, AxolotlShoulderAccessor {
     // This interface is a marker interface that combines the functionality of AxolotlTameableAccessor and AxolotlShoulderAccessor.
     // It does not add any new methods or properties.
 }
