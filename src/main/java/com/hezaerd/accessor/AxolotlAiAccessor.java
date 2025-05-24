7c0295f277b8017dba0cdb4bd63616a972d9fdae
Swann <swann.rouanet@gmail.com>
2025-05-24 22:03:40 +0200

    feat: add axolotl follow owner ai

---
+++ src/main/java/com/hezaerd/accessor/AxolotlAiAccessor.java
@@ -0,0 +1,7 @@
+package com.hezaerd.accessor;
+
+public interface AxolotlAiAccessor {
+    boolean betteraxolotls$cannotFollowOwner();
+    boolean betteraxolotls$shouldTryTeleportToOwner();
+    void betteraxolotls$tryTeleportToOwner();
+}
