34023c863a5cfc597b212a7c3dfe031361e76dbd
Swann <swann.rouanet@gmail.com>
2025-05-26 16:02:41 +0200

    feat: make copper toxic for axolotl

--- src/main/java/com/hezaerd/mixin/AxolotlInteractionsMixin.java
+++ src/main/java/com/hezaerd/mixin/AxolotlInteractionsMixin.java
@@ -2,9 +2,12 @@ package com.hezaerd.mixin;
 
 import com.hezaerd.accessor.AxolotlShoulderAccessor;
 import com.hezaerd.accessor.AxolotlTameableAccessor;
+import com.hezaerd.registry.tag.ModItemTags;
 import net.minecraft.entity.Bucketable;
 import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -34,97 +37,129 @@ public abstract class AxolotlInteractionsMixin extends AnimalEntity {
     protected AxolotlInteractionsMixin(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
-    
+
     @Override
     public ActionResult interactMob(PlayerEntity player, Hand hand) {
         ItemStack itemStack = player.getStackInHand(hand);
-        
-        AxolotlTameableAccessor tameableAccessor = (AxolotlTameableAccessor) this;
-        AxolotlShoulderAccessor shoulderAccessor = (AxolotlShoulderAccessor) this;
 
-        // Taming related interactions
-        if (tameableAccessor.betteraxolotls$isTamed()) {
-            // Owner only interactions
-            boolean isOwner = tameableAccessor.betteraxolotls$getOwner() == player;
-            if (isOwner) {
-                Optional<ActionResult> result = Bucketable.tryBucket(player, hand, (AxolotlEntity)(Object)this);
-                if (result.isPresent()) {
-                    return result.get();
-                }
-                
-                // Empty-hand interactions
-                if (itemStack.isEmpty() && shoulderAccessor.betteraxolotls$isReadyToSitOnPlayer() && !this.isBaby() && !player.isSubmergedInWater()) {
-                    if (!this.getWorld().isClient) {
-                        ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-                        if (shoulderAccessor.betteraxolotls$mountOnto(serverPlayer)) {
-                            SoundEvent soundEvent = this.isSubmergedInWater() ?
-                                    SoundEvents.ENTITY_AXOLOTL_SPLASH :
-                                    SoundEvents.ENTITY_BAT_TAKEOFF;
-                            this.getWorld().playSound(
-                                    null,
-                                    this.getX(), this.getY(), this.getZ(),
-                                    soundEvent,
-                                    this.getSoundCategory(),
-                                    1.0F,
-                                    1.0F + (this.getWorld().random.nextFloat() - this.getWorld().random.nextFloat()) * 0.2F
-                            );
-                            return ActionResult.SUCCESS_SERVER;
-                        } else {
-                            return ActionResult.FAIL;
-                        }
-                    }
-                }
+        AxolotlTameableAccessor tameable = (AxolotlTameableAccessor) this;
+        AxolotlShoulderAccessor shoulder = (AxolotlShoulderAccessor) this;
+
+        // Poisonous food interaction
+        if (tryPoisonousFood(player, hand, itemStack)) return ActionResult.SUCCESS_SERVER;
+
+        // Tamed interactions
+        if (tameable.betteraxolotls$isTamed()) {
+            if (tameable.betteraxolotls$getOwner() == player) {
+                Optional<ActionResult> bucketResult = tryBucket(player, hand);
+                if (bucketResult.isPresent()) return bucketResult.get();
+
+                if (tryShoulderMount(player, itemStack, shoulder)) return ActionResult.SUCCESS_SERVER;
             }
         } else {
-            Optional<ActionResult> result = Bucketable.tryBucket(player, hand, (AxolotlEntity)(Object)this);
-            if (result.isPresent()) {
-                return result.get();
-            }
-            
-            if (!this.getWorld().isClient && itemStack.isIn(ItemTags.AXOLOTL_FOOD) && tameableAccessor.betteraxolotls$isTameable()) {
-                itemStack.decrementUnlessCreative(1, player);
-                if (!this.isSilent()) {
-                    SoundEvent soundEvent = this.isSubmergedInWater() ? 
-                            SoundEvents.ENTITY_AXOLOTL_IDLE_WATER : 
-                            SoundEvents.ENTITY_AXOLOTL_IDLE_AIR;
-                    
-                    this.getWorld().playSound(
-                            null, 
-                            this.getX(), this.getY(), this.getZ(),
-                            soundEvent,
-                            this.getSoundCategory(),
-                            1.0F,
-                            1.0F + (this.getWorld().random.nextFloat() - this.getWorld().random.nextFloat()) * 0.2F
-                    );
+            Optional<ActionResult> bucketResult = tryBucket(player, hand);
+            if (bucketResult.isPresent()) return bucketResult.get();
+
+            if (tryTame(player, itemStack, tameable)) return ActionResult.SUCCESS_SERVER;
+        }
+
+        // Breeding interaction
+        if (tryBreed(player, hand, itemStack)) return ActionResult.SUCCESS_SERVER;
+
+        return super.interactMob(player, hand);
+    }
+
+    @Unique
+    private boolean tryPoisonousFood(PlayerEntity player, Hand hand, ItemStack itemStack) {
+        if (!this.getWorld().isClient && itemStack.isIn(ModItemTags.AXOLOTL_POISONOUS_FOOD) && this.canEat()) {
+            this.eat(player, hand, itemStack);
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 600));
+            return true;
+        }
+        return false;
+    }
+
+    @Unique
+    private Optional<ActionResult> tryBucket(PlayerEntity player, Hand hand) {
+        return Bucketable.tryBucket(player, hand, (AxolotlEntity) (Object) this);
+    }
+
+    @Unique
+    private boolean tryShoulderMount(PlayerEntity player, ItemStack itemStack, AxolotlShoulderAccessor shoulder) {
+        if (itemStack.isEmpty() && shoulder.betteraxolotls$isReadyToSitOnPlayer() && !this.isBaby() && !player.isSubmergedInWater()) {
+            if (!this.getWorld().isClient) {
+                ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+                if (shoulder.betteraxolotls$mountOnto(serverPlayer)) {
+                    playMountSound();
+                    return true;
                 }
-                tameableAccessor.betteraxolotls$tryTame(player);
-                return ActionResult.SUCCESS_SERVER;
             }
         }
-        
-        // Breeding interaction
+        return false;
+    }
+
+    @Unique
+    private void playMountSound() {
+        SoundEvent soundEvent = this.isSubmergedInWater() ?
+                SoundEvents.ENTITY_AXOLOTL_SPLASH :
+                SoundEvents.ENTITY_BAT_TAKEOFF;
+        this.getWorld().playSound(
+                null,
+                this.getX(), this.getY(), this.getZ(),
+                soundEvent,
+                this.getSoundCategory(),
+                1.0F,
+                1.0F + (this.getWorld().random.nextFloat() - this.getWorld().random.nextFloat()) * 0.2F
+        );
+    }
+
+    @Unique
+    private boolean tryTame(PlayerEntity player, ItemStack itemStack, AxolotlTameableAccessor tameable) {
+        if (!this.getWorld().isClient && itemStack.isIn(ItemTags.AXOLOTL_FOOD) && tameable.betteraxolotls$isTameable()) {
+            itemStack.decrementUnlessCreative(1, player);
+            playTameSound();
+            tameable.betteraxolotls$tryTame(player);
+            return true;
+        }
+        return false;
+    }
+
+    @Unique
+    private void playTameSound() {
+        if (!this.isSilent()) {
+            SoundEvent soundEvent = this.isSubmergedInWater() ?
+                    SoundEvents.ENTITY_AXOLOTL_IDLE_WATER :
+                    SoundEvents.ENTITY_AXOLOTL_IDLE_AIR;
+            this.getWorld().playSound(
+                    null,
+                    this.getX(), this.getY(), this.getZ(),
+                    soundEvent,
+                    this.getSoundCategory(),
+                    1.0F,
+                    1.0F + (this.getWorld().random.nextFloat() - this.getWorld().random.nextFloat()) * 0.2F
+            );
+        }
+    }
+
+    @Unique
+    private boolean tryBreed(PlayerEntity player, Hand hand, ItemStack itemStack) {
         if (this.isBreedingItem(itemStack)) {
             int breedingAge = this.getBreedingAge();
             if (!this.getWorld().isClient && breedingAge == 0 && this.canEat()) {
                 this.eat(player, hand, itemStack);
                 this.lovePlayer(player);
                 this.playEatSound();
-                return ActionResult.SUCCESS_SERVER;
+                return true;
             }
-            
             if (this.isBaby()) {
                 this.eat(player, hand, itemStack);
                 this.growUp(toGrowUpAge(-breedingAge), true);
                 this.playEatSound();
-                return ActionResult.SUCCESS;
+                return true;
             }
-            
-            if (this.getWorld().isClient) {
-                return ActionResult.CONSUME;
-            }
+            return this.getWorld().isClient; // Will return CONSUME below
         }
-        
-        return super.interactMob(player, hand);
+        return false;
     }
     
     @Override
